2c13071537d9aa3253cca72fe5c1a55e66cd670e
Aanviag <aanviag15@gmail.com>
2026-08-28 09:44:09 -0400

    Update main.py

--- Data Science & AI/Cardio_Tree_Classifier/main.py
+++ Data Science & AI/Cardio_Tree_Classifier/main.py
@@ -1,118 +1,104 @@
-#Talking Data Starter Code
-
-#Part 2 Setting up the program
 import pandas as pd
-import matplotlib.pyplot as plt
+import GWCutilities as util
 
 pd.set_option('display.max_columns', None)
 pd.set_option('max_colwidth', None)
 
-movieData = pd.read_csv('./rotten_tomatoes_movies.csv')
-favMovie = "Sleeping Beauty"
-print("My favorite movie is: " + favMovie)
+print("\n-----\n")
+
+#Create a variable to read the dataset
+df = pd.read_csv("heartDisease_2020_sampling.csv")
+
+print("We will be performing data analysis on this Indicators of Heart Disease Dataset. Here is a sample of it: \n")
+
+#Print the dataset's first five rows
+
+
+input("\n Press Enter to continue.\n")
 
 
 
-#Part 3 Investigate the data
-#print(movieData.head())
-#print(movieData["movie_title"])
+#Data Cleaning
+#Label encode the dataset
+df = util.labelEncoder(df, ["HeartDisease", "GenHealth", "Smoking", "AlcoholDrinking", "PhysicalActivity", "Sex", "AgeCategory"])
+print(df.head())
 
+print("\nHere is a preview of the dataset after label encoding. \n")
 
-#Part 4 Filter data
-print("\nThe data for my favorite movie is:\n")
-#Create a new variable to store your favorite movie information
-favMovieBooleanList = movieData["movie_title"] == favMovie
-#print(favMovieBooleanList)
+input("\nPress Enter to continue.\n")
 
-favMovieData = movieData.loc[favMovieBooleanList]
-print(favMovieData)
+#One hot encode the dataset
+df = util.oneHotEncoder(df, ["Race"])
+print(df.head())
 
-
-print("\n\n")
-
-#Create a new variable to store a new data set with a certain genre
-fantasyMovieBooleanList = movieData["genres"].str.contains("Fantasy")
-
-fantasyMovieData = movieData.loc[fantasyMovieBooleanList]
-
-
-
-
-numOfMovies = fantasyMovieData.shape[0]
-
-
-print("We will be comparing " + favMovie +
-      " to other movies under the genre Fantasy in the data set.\n")
-print("There are " + str(numOfMovies) + " movies under the category Fantasy.")
-
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-input("Press enter to see more information about how " + favMovie +
-      " compares to other movies in this genre.\n")
-
-
-#Part 5 Describe data
-#min
-min = fantasyMovieData["audience_rating"].min()
-print("The min audience rating of the data set is: " + str(min))
-print(favMovie + " is rated 72 points higher than the lowest rated movie.")
-print()
-
-#find max
-max = fantasyMovieData["audience_rating"].max()
-print("The max audience rating of the data set is: " + str(max))
-print(favMovie + " is rated 20 points lower than the highest rated movie.")
-print()
-
-#find mean
-mean = fantasyMovieData["audience_rating"].mean()
-print("The mean audience rating of the data set is: " + str(mean))
-print(favMovie + " is high than the mean movie rating.")
-
-#find median
-median = fantasyMovieData["audience_rating"].median()
-print("The median audience rating of the data set is: " + str(median))
-print(favMovie + " is higher than the median movie rating.")
-
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
-input("Press enter to see data visualizations.\n")
-
-#Part 6 Create graphs
-#Create histogram
-plt.hist(fantasyMovieData["audience_rating"], range = (0, 100), bins = 20)
-
-#Adds labels and adjusts histogram
-plt.grid(True)
-plt.title("Audience Ratings of Fantasy Movies Histogram")
-plt.xlabel("Audience Ratings")
-plt.ylabel("Number of Fantasy Ratings")
-
-#Prints interpretation of histogram
 print(
-  "According to the histogram, the audience rating of 60 got the highest of number of fantasy movie ratings. The graph looks like an city line with heights of various length. The edges of the graph where the audience rating is 20 and 100 have rate the least fantasy movie ratings. "
+    "\nHere is a preview of the dataset after one hot encoding. This will be the dataset used for data analysis: \n"
 )
-print()
-
-#Show histogram
-plt.show()
-input("Press enter to see the next data visualization.\n")
-plt.close()
-
-#Create scatterplot
-plt.scatter(data = fantasyMovieData, x = "audience_rating", y ="critic_rating")
-#Adds labels and adjusts scatterplot
-plt.grid(True)
-plt.title("Audience Rating vs. Critic Rating")
-plt.xlabel("Audience Rating")
-plt.ylabel("Critic Rating")
-plt.xlim(0, 100)
-plt.ylim(0, 100)
-
-#Prints interpretation of scatterplot
-print( "Accroding to the scatterplot, there is a positive correlation between the audience and critic rating. AS, the audience rating increases, so does the number of critic ratings. There are a few outliers when the audience rating is near 100, there are little to no critic ratings.")
-print()
 
 
-#Show scatterplot
-plt.show()
+input("\nPress Enter to continue.\n")
 
-print("\nThank you for reading through my data analysis!")
+
+
+#Creates and trains Decision Tree Model
+from sklearn.model_selection import train_test_split
+X = df.drop("HeartDisease", axis = 1)
+y = df["HeartDisease"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 3)
+print(X_train.head())
+
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(max_depth = 2, class_weight = 'balanced')
+clf = clf.fit(X_train, y_train)
+
+
+
+
+
+
+#Test the model with the testing data set and prints accuracy score
+test_predictions = clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+test_acc = accuracy_score(y_test, test_predictions)
+
+print("\nThe accuracy with the testing data set of the Descision Tree is: " + str(test_acc))
+
+
+#Prints the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, test_predictions, labels= [1, 0])
+print("The confusion matrix of the tree is: ")
+print(cm)
+
+
+
+
+
+#Test the model with the training data set and prints accuracy score
+train_predictions = clf.predict(X_train)
+
+from sklearn.metrics import accuracy_score
+train_acc = accuracy_score(y_train, train_predictions)
+
+print("\nThe accuracy with the training data set of the Descision Tree is: " + str(train_acc))
+
+
+
+input("\nPress Enter to continue.\n")
+
+
+
+#Prints another application of Decision Trees and considerations
+print("\nBelow is another application of decision trees and considerations for using them:\n")
+print("Data Decision Trees can be used in restuarants to recommend food options to a user. One exmaple, is a pizza business, where the model can suggest pizza topics or options based on someone's tastes, options, etc.. One thing to make sure would be to feed unbaised data to the tree and make it not suggesting information based on location, race, etc..")
+
+
+
+
+#Prints a text representation of the Decision Tree
+print("\nBelow is a text representation of how the Decision Tree makes choices:")
+input("Press Enter to continue.\n")
+
+util.printTree(clf, X.columns)
